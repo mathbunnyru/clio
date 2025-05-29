492038b7ccf519704279587974020ebfdd6f8ce6
Ayaz Salikhov <asalikhov@ripple.com>
2025-05-29 18:47:27 +0100

    Add nice mock

--- tests/common/util/MockRandomGenerator.hpp
+++ tests/common/util/MockRandomGenerator.hpp
@@ -20,7 +20,12 @@
 #pragma once
 #include "util/Random.hpp"
 
-struct MockRandomGenerator : public util::RandomGeneratorInterface {
+#include <gtest/gtest.h>
+
+struct MockRandomGeneratorImpl : public util::RandomGeneratorInterface {
     MOCK_METHOD(size_t, uniform, (size_t min, size_t max), (override));
     MOCK_METHOD(void, setSeed, (SeedType seed), (override));
 };
+
+using MockRandomGenerator = testing::NiceMock<MockRandomGeneratorImpl>;
+using StrictMockRandomGenerator = testing::StrictMock<MockRandomGeneratorImpl>;
