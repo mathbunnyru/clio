75bb96dde4aeb9d129926cb3ba4712317d3e0798
Ayaz Salikhov <asalikhov@ripple.com>
2025-05-30 15:58:54 +0100

    Better name and quotes

--- .github/scripts/prepare-release-artifacts.sh
+++ .github/scripts/prepare-release-artifacts.sh
@@ -6,13 +6,13 @@ BINARY_NAME = "clio_server"
 
 GITHUB_ARTIFACTS = "$(ls)"
 
-for artifact_dir in "${GITHUB_ARTIFACTS}; do
-    pushd "${artifact_dir}" || exit 1
-    zip -r "../${artifact_dir}.zip" ./${BINARY_NAME}
+for artifact_name in "${GITHUB_ARTIFACTS}; do
+    pushd "${artifact_name}" || exit 1
+    zip -r "../${artifact_name}.zip" ./${BINARY_NAME}
     popd || exit 1
 
-    rm "${artifact_dir}/${BINARY_NAME}"
-    rm -r "${artifact_dir}"
+    rm "${artifact_name}/${BINARY_NAME}"
+    rm -r "${artifact_name}"
 
-    sha256sum ./${artifact_dir}.zip > ./${artifact_dir}.zip.sha256sum
+    sha256sum "./${artifact_name}.zip" > "./${artifact_name}.zip.sha256sum"
 done
