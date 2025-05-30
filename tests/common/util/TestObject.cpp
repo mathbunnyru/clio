ce42f3be128248ad6539311cd6d57d7d17b3885e
Ayaz Salikhov <asalikhov@ripple.com>
2025-05-30 14:23:23 +0100

    One liner

--- tests/common/util/TestObject.cpp
+++ tests/common/util/TestObject.cpp
@@ -502,9 +502,8 @@ createOfferLedgerObject(
     offer.setFieldH256(ripple::sfBookDirectory, ripple::uint256{dirId});
     offer.setFieldH256(ripple::sfPreviousTxnID, ripple::uint256{});
     offer.setFieldU32(ripple::sfPreviousTxnLgrSeq, 0);
-    if (domain.has_value()) {
+    if (domain.has_value())
         offer.setFieldH256(ripple::sfDomainID, ripple::uint256{*domain});
-    }
     return offer;
 }
 
