d646bf77af30020d9445123328e90b9d6d069db6
Ayaz Salikhov <asalikhov@ripple.com>
2025-07-24 17:40:40 +0100

    chore: Do not add sha256 to releases

--- .github/scripts/prepare-release-artifacts.sh
+++ .github/scripts/prepare-release-artifacts.sh
@@ -19,6 +19,4 @@ for artifact_name in $(ls); do
 
     rm "${artifact_name}/${BINARY_NAME}"
     rm -r "${artifact_name}"
-
-    sha256sum "./${artifact_name}.zip" > "./${artifact_name}.zip.sha256sum"
 done
