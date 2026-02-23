6938a2f417e438d1b6da5129d2a083e8cfca6864
Ayaz Salikhov <asalikhov@ripple.com>
2026-02-23 14:34:50 +0000

    style: Remove readability-identifier-naming where not needed

--- src/util/build/Build.cpp
+++ src/util/build/Build.cpp
@@ -44,37 +44,36 @@ static constexpr char kBUILD_DATE[] = BUILD_DATE;
 std::string const&
 getClioVersionString()
 {
-    static std::string const value = kVERSION_STRING;  // NOLINT(readability-identifier-naming)
-    return value;
+    static std::string const kVALUE = kVERSION_STRING;
+    return kVALUE;
 }
 
 std::string const&
 getClioFullVersionString()
 {
-    static std::string const kVALUE =
-        "clio-" + getClioVersionString();  // NOLINT(readability-identifier-naming)
+    static std::string const kVALUE = "clio-" + getClioVersionString();
     return kVALUE;
 }
 
 std::string const&
 getGitCommitHash()
 {
-    static std::string const value = kGIT_COMMIT_HASH;  // NOLINT(readability-identifier-naming)
-    return value;
+    static std::string const kVALUE = kGIT_COMMIT_HASH;
+    return kVALUE;
 }
 
 std::string const&
 getGitBuildBranch()
 {
-    static std::string const value = kGIT_BUILD_BRANCH;  // NOLINT(readability-identifier-naming)
-    return value;
+    static std::string const kVALUE = kGIT_BUILD_BRANCH;
+    return kVALUE;
 }
 
 std::string const&
 getBuildDate()
 {
-    static std::string const value = kBUILD_DATE;  // NOLINT(readability-identifier-naming)
-    return value;
+    static std::string const kVALUE = kBUILD_DATE;
+    return kVALUE;
 }
 
 }  // namespace util::build
