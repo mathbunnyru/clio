ca5c2f5450a206b24d98d682c79ce1c1eae45f2d
Ayaz Salikhov <asalikhov@ripple.com>
2026-02-25 15:12:33 +0000

    Set CPACK_STRIP_FILES

--- cmake/ClioPackage.cmake
+++ cmake/ClioPackage.cmake
@@ -2,6 +2,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/ClioVersion.cmake")
 
 set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/clio")
 set(CPACK_PACKAGE_VERSION "${CLIO_VERSION}")
+set(CPACK_STRIP_FILES TRUE)
 
 include(pkg/deb)
 include(CPack)
