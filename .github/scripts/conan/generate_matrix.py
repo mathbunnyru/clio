52282b1947945604ea5132172dcb3b6f3973b161
Ayaz Salikhov <mathbunnyru@gmail.com>
2026-06-10 23:03:53 +0100

    Update image

--- .github/scripts/conan/generate_matrix.py
+++ .github/scripts/conan/generate_matrix.py
@@ -3,7 +3,7 @@ import itertools
 import json
 
 LINUX_OS = ["heavy", "heavy-arm64"]
-LINUX_CONTAINERS = ['{ "image": "ghcr.io/xrplf/xrpld/nix-ubuntu:sha-577d745" }']
+LINUX_CONTAINERS = ['{ "image": "ghcr.io/xrplf/xrpld/nix-ubuntu:sha-dd0b675" }']
 LINUX_COMPILERS = ["gcc", "clang"]
 
 MACOS_OS = ["macos15"]
